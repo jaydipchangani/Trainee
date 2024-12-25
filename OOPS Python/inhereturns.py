510a7764fced617e2f417cbe346532551f417c22
jaydip <pjaydip956@gmail.com>
2024-12-25 17:26:07 +0530

    oops

---
+++ OOPS Python/inhereturns.py
@@ -0,0 +1,23 @@
+class human:
+    def __init__(self,spe):
+        self.spe=spe
+    
+    def walk(self):
+        print("Human can walk")
+    
+class men(human):
+    def walk(self):
+        print("Men can speak")
+
+class women(human):
+    def walk(self):
+        print("Women can speak")
+        
+
+human1=human("homo")
+man1=men("Men")
+woman1=women("Women")
+
+human1.speak()
+man1.speak()
+woman1.speak()
