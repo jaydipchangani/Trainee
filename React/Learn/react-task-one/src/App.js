48088855bd68dad4e985969a606dc59170e192ec
jaydipchangani <pjaydip956@gmail.com>
2025-02-18 15:33:27 +0530

    structure ready

--- React/Learn/react-task-one/src/App.js
+++ React/Learn/react-task-one/src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './component/ProfileCard.css';
+import React from 'react';
+import ProfileCard from './component/ProfileCard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='mainComponent'>
+    
+    <ProfileCard />
+    <ProfileCard />
+    <ProfileCard />
+    <ProfileCard />
     </div>
   );
 }
---
+++ React/Learn/react-task-one/src/component/ProfileCard.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const ProfileCard = () => {
+    return (
+        <>
+            <div className="profile-card">
+                <img src="logo192.png" alt="Profile Pic" className='imageProfile' />
+                <h2>Jaydip Changani</h2>
+                <p>Software Developer Intern</p>
+                <a href="https://in.linkedin.com/in/jaydip-changani">View Profile</a>
+            </div>
+        
+        </>
+    )
+    
+}
+export default ProfileCard;
