e55f2690112e0ad00f8681c9a27158e43bc77efb
jaydipchangani <pjaydip956@gmail.com>
2025-02-18 12:14:46 +0530

    app.js change add name

--- React/Learn/my-react-app/src/App.jsx
+++ React/Learn/my-react-app/src/App.jsx
@@ -4,10 +4,14 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const username = "Jaydip";
+  const isHero = false;
+
   return (
     <div>
-      <h1>Hello, React! 🚀</h1>
-      <p>Welcome to your first React app.</p>
+      <h1>Hello, {username}!</h1>
+      <p>{isHero ? "Welcome to Gotham City!" : "Welcome to React!"}</p>
+      <button className="btn">Click Me</button>
     </div>
   );
 }
