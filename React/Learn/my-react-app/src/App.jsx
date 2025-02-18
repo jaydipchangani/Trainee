dc117ee20e98453eb3873c1ea378f3801b5033b1
jaydipchangani <pjaydip956@gmail.com>
2025-02-18 10:44:39 +0530

    init react learn

---
+++ React/Learn/my-react-app/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React! 🚀</h1>
+      <p>Welcome to your first React app.</p>
+    </div>
+  );
+}
+
+export default App
