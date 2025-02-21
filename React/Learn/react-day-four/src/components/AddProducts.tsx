e9e59aebd905cc37c536e27ee7db363ff202c46a
jaydipchangani <pjaydip956@gmail.com>
2025-02-21 12:20:33 +0530

    Add Bootstrap and React Router for improved styling and navigation; create AddProducts and Products components

---
+++ React/Learn/react-day-four/src/components/AddProducts.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AddProducts(){
+    return (
+        <div>
+            <h1>Add Products Page</h1>
+           
+        </div>
+    )
+}
+
+export default AddProducts;
