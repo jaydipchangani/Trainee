5115420be9e08a08035885a4d4f9ffcb6c2f5ef2
jaydipchangani <pjaydip956@gmail.com>
2025-02-21 16:38:34 +0530

    Refactor Sidebar width and add custom styles for improved navigation experience

--- React/Learn/product-management/src/components/Sidebar.tsx
+++ React/Learn/product-management/src/components/Sidebar.tsx
@@ -3,9 +3,10 @@ import { Nav } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 const Sidebar: React.FC = () => {
   return (
-    <Nav className="flex-column bg-primary text-white p-3" style={{ height: "100vh", width: "250px" }}>
+    <Nav className="flex-column bg-primary text-white p-3" style={{ height: "100vh", width: "200px" }}>
       <Nav.Link as={Link} to="/products" className="text-white mb-2" active>
         Products
       </Nav.Link>
