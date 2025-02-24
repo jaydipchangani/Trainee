9844227a4f01e02957154982c78779adc77b4d4e
jaydipchangani <pjaydip956@gmail.com>
2025-02-24 11:31:38 +0530

    Integrate Ant Design components: replace Bootstrap Navbar with Ant Design Header; implement collapsible Sidebar with Ant Design Menu for improved UI/UX.

--- React/Learn/product-management/src/components/Header.tsx
+++ React/Learn/product-management/src/components/Header.tsx
@@ -1,12 +1,40 @@
-import React from "react";
-import { Navbar } from "react-bootstrap";
+import React from 'react';
+import {Layout, Menu,Typography  } from 'antd';
 
-const Header: React.FC = () => {
+const { Header} = Layout;
+const { Title } = Typography;
+
+const items = [
+  { key: '1', label: 'Home', path: '/' },
+  { key: '2', label: 'Products', path: '/products' },
+  { key: '3', label: 'Add Products', path: '/add' },
+];
+
+const App: React.FC = () => {
+  
   return (
-    <Navbar bg="dark" variant="dark" className="px-3">
-      <Navbar.Brand href="/products">Product Management</Navbar.Brand>
-    </Navbar>
+    <Layout>
+      
+      <Header
+        style={{
+         
+          top: 0,
+          zIndex: 1,
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          color:'blue',
+          padding: 0
+        }}
+      >
+        <div className="demo-logo" />
+        <Title level={3} style={{color:'white'}}>Products Managment</Title>
+        
+      </Header>
+
+    </Layout>
   );
 };
 
-export default Header;
+export default App;
