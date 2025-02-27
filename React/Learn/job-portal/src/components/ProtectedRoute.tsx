8d3cea45b914d5891e671251b19f65b98a539359
jaydipchangani <pjaydip956@gmail.com>
2025-02-27 10:34:44 +0530

    replace removed

--- React/Learn/job-portal/src/components/ProtectedRoute.tsx
+++ React/Learn/job-portal/src/components/ProtectedRoute.tsx
@@ -1,9 +1,8 @@
 import { Navigate } from "react-router-dom";
-import React from "react";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
   const isAuthenticated = localStorage.getItem("user");
-  return isAuthenticated ? children : <Navigate to="/" replace />;
+  return isAuthenticated ? children : <Navigate to="/"  />;
 };
 
 export default ProtectedRoute;
