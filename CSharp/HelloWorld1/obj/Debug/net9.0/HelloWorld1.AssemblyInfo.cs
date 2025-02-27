bf3caa3cc0173ad60e387de9bd14f613df653c8f
jaydipchangani <pjaydip956@gmail.com>
2025-02-27 12:32:40 +0530

    C# testing

--- CSharp/HelloWorld1/obj/Debug/net9.0/HelloWorld1.AssemblyInfo.cs
+++ CSharp/HelloWorld1/obj/Debug/net9.0/HelloWorld1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7510949fd9b5defffea9f8e22e89ac23fc4bc781")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+432fed3512157cce6b1eeaabd814c0fd877f62d5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
