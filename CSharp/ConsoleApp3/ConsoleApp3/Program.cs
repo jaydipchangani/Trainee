72a1ce16cea6fca4d5f958ecbda10265e79daf22
jaydipchangani <pjaydip956@gmail.com>
2025-03-05 13:06:14 +0530

    try catch and files practice

--- CSharp/ConsoleApp3/ConsoleApp3/Program.cs
+++ CSharp/ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,17 +1,65 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp3
 {
     class Program
     {
+        enum Marks {Best,Good,Avg, Poor }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the marks");
+            int marks = Convert.ToInt32(Console.ReadLine());
+
+            if (marks > 100 || marks < 0)
+            {
+                throw new ArithmeticException("Marks should be between 0 to 100");
+            }
+            else
+            {
+
+                if (marks > 90)
+                {
+                    Console.WriteLine(Marks.Best);
+                }
+                else if (marks > 80)
+                {
+                    Console.WriteLine(Marks.Good);
+                }
+                else if (marks > 70)
+                {
+                    Console.WriteLine(Marks.Avg);
+                }
+                else
+                {
+                    Console.WriteLine(Marks.Poor);
+                }
+            }
+
+            string name = "Jaydip";
+
+            File.WriteAllText("test.txt", name);
+
+            Console.WriteLine(File.ReadAllText("test.txt"));
+
+            File.AppendAllText("test.txt", " Changani");
+            Console.WriteLine(File.ReadAllText("test.txt"));
+
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            try
+            {
+                Console.WriteLine(arr[11]);
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Finally block");    
+            }
+
+
         }
     }
 }
