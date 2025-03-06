89f23cbf821890bdf2136aab4bae3506aa1d97d8
jaydipchangani <pjaydip956@gmail.com>
2025-03-06 17:11:35 +0530

    UI header done

--- React/Learn/gather-nexus/src/components/Header.tsx
+++ React/Learn/gather-nexus/src/components/Header.tsx
@@ -1,10 +1,11 @@
 import React, { useContext } from "react";
-import { Layout, Tooltip } from "antd";
+import { Layout, Tooltip,Typography } from "antd";
 import { AuthContext } from "../context/AuthContext";
 import { getToken } from "../utils/storage";
 import { LogoutOutlined } from "@ant-design/icons";
 
 const { Header } = Layout;
+const { Title } = Typography;
 
 const AppHeader: React.FC = () => {
   const auth = useContext(AuthContext);
@@ -14,15 +15,19 @@ const AppHeader: React.FC = () => {
   const userName = user?.name || "Guest"; // Default to "Guest" if no user
 
   return (
-    <Header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", background: "#001529", color: "#fff", padding: "0 20px" }}>
-      <h2 style={{ color: "#fff" }}>Gather.nexus</h2>
+    <Header style={{ display: "flex", justifyContent: "space-between", alignItems: "center", background: "white", color: "#fff", padding: "0 20px" }}>
+      <div className="logo">
+          <Title level={3} style={{ color: '#2E8B57', margin: 0 }}>
+            GATHER<span style={{ fontSize: '16px', fontWeight: 'normal' }}>.nexus</span>
+          </Title>
+        </div>
       <div style={{ display: "flex", alignItems: "center", gap: "15px" }}>
-        <span style={{ color: "#fff", fontWeight: "bold" }}> {userName}</span>
+        <span style={{ color: "#000000", fontWeight: "bold" }}> {userName}</span>
         {getToken() && (
     <Tooltip title="Logout">
     <LogoutOutlined 
       onClick={auth?.logout} 
-      style={{ fontSize: "18px", color: "#fff", cursor: "pointer" }} 
+      style={{ fontSize: "18px", color: "#000000", cursor: "pointer" }} 
     />
   </Tooltip>
 )}
