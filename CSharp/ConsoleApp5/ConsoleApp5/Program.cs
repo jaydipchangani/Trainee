8494aa42ea927375f6bd4957c571ec99be9dedf4
jaydipchangani <pjaydip956@gmail.com>
2025-03-10 15:00:42 +0530

    Generics methods

---
+++ CSharp/ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Print <T>(T values)
+            {
+                Console.WriteLine(values);
+            }
+
+        static void Main()
+        {
+            Print<string>("JD");
+            Print<int>(12);
+            Print<double>(2.65956);
+        }
+    }
+}
