13ecd873716d22547b05e65bb64989f386f0ea4f
jaydipchangani <pjaydip956@gmail.com>
2025-03-11 14:34:18 +0530

    basic form added

---
+++ CSharp/MyFirstAspNetApp/MyFirstAspNetApp/Controllers/ProductFormController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstAspNetApp.Controllers
+{
+    public class ProductFormController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(string username)
+        {
+            ViewData["Message"] = "Hello " + username + " !!";
+            return View();
+        }
+    }
+}
+   
