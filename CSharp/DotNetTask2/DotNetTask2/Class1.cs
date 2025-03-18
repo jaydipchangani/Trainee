47d7218747533a7cc4dd1ca3700e7d4ee957a779
jaydipchangani <pjaydip956@gmail.com>
2025-03-18 16:01:10 +0530

    validation added

---
+++ CSharp/DotNetTask2/DotNetTask2/Class1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DotNetTask2
+{
+    static class Class1
+    {
+        enum designantion { trainee, developer, qa , manager, projectmanager}
+
+        public static bool isNull(this string s)
+        {
+            if (string.IsNullOrEmpty(s))
+            {
+                return true ;
+            }
+            return false;
+        }
+
+        public static bool isValidEmail(this string e)
+        {
+            Regex validateEmailRegex = new Regex("^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$");
+
+            if (validateEmailRegex.IsMatch(e))
+            {
+                return false ;
+            }
+            return true;
+        }
+
+        public static bool isValidPhone(this string m)
+        {
+            if (m.Length == 10)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool isValidSalary(this Int32 s)
+        {
+            if (s >= 10000 && s <= 50000)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        public static bool isValidDesignantion(this string d)
+        {
+            if(Enum.IsDefined(typeof(designantion), d))
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
--- CSharp/DotNetTask2/DotNetTask2/Program.cs
+++ CSharp/DotNetTask2/DotNetTask2/Program.cs
@@ -1,26 +1,100 @@
 ﻿
-Console.WriteLine("Task 1");
-File.WriteAllText("test.txt", "Hello Welcome to task 2");
-Console.WriteLine("File  created a file and add some text");
+//Console.WriteLine("Task 1");
+//File.WriteAllText("test.txt", "Hello Welcome to task 2");
+//Console.WriteLine("File  created a file and add some text");
 
-Console.WriteLine("\nTask 2");
-File.WriteAllText("test.txt", "Hello Welcome to task 2 subtask 2-2");
-string fileData = File.ReadAllText("test.txt");
-Console.WriteLine(fileData);
+//Console.WriteLine("\nTask 2");
+//File.WriteAllText("test.txt", "Hello Welcome to task 2 subtask 2-2");
+//string fileData = File.ReadAllText("test.txt");
+//Console.WriteLine(fileData);
 
-Console.WriteLine("\nTask 3");
-string[] arr = { "a", "b", "c", "d", "e", "f" };
-File.WriteAllLines("test.txt", arr);
-string arrayFile = File.ReadAllText("test.txt");
-Console.WriteLine(arrayFile);
+//Console.WriteLine("\nTask 3");
+//string[] arr = { "a", "b", "c", "d", "e", "f" };
+//File.WriteAllLines("test.txt", arr);
+//string arrayFile = File.ReadAllText("test.txt");
+//Console.WriteLine(arrayFile);
 
-Console.WriteLine("\nTask 4");
-File.AppendAllText("test.txt", "Hello How are you ?");
-Console.WriteLine(File.ReadAllText("test.txt"));
+//Console.WriteLine("\nTask 4");
+//File.AppendAllText("test.txt", "Hello How are you ?");
+//Console.WriteLine(File.ReadAllText("test.txt"));
 
-Console.WriteLine("\nTask 5");
-Console.WriteLine(File.ReadAllLines("test.txt").First());
+//Console.WriteLine("\nTask 5");
+//Console.WriteLine(File.ReadAllLines("test.txt").First());
 
-Console.WriteLine("\nTask 6");
-var len=File.ReadAllLines("test.txt").Length;
-Console.WriteLine(len);
+//Console.WriteLine("\nTask 6");
+//var len=File.ReadAllLines("test.txt").Length;
+//Console.WriteLine(len);
+
+// User Data
+
+using System.ComponentModel;
+using DotNetTask2;
+
+Console.WriteLine("Enter First Name:");
+string firstName=Console.ReadLine();
+
+while(firstName.isNull())
+{
+    Console.WriteLine("Enter First Name Again");
+    firstName = Console.ReadLine();
+}
+
+Console.WriteLine("Enter Last Name:");
+string lasttName = Console.ReadLine();
+
+ while(lasttName.isNull())
+{
+    Console.WriteLine("Enter Last Name Again");
+    lasttName = Console.ReadLine();
+}
+
+
+Console.WriteLine("Enter Gender:");
+string gender = Console.ReadLine();
+
+while (gender.isNull())
+{
+    Console.WriteLine("Enter Gender Again");
+    gender = Console.ReadLine();
+}
+
+
+Console.WriteLine("Enter Email Address:");
+string email = Console.ReadLine();
+
+while (email.isValidEmail())
+{
+    Console.WriteLine("Email is not Valid, Enter Email Again");
+    email = Console.ReadLine();
+}
+
+Console.WriteLine("Enter Phone Number:");
+string phone = Console.ReadLine();
+
+while (phone.isValidPhone())
+{
+    Console.WriteLine("Enter Phone Again");
+    phone = Console.ReadLine();
+}
+
+Console.WriteLine("Enter Designation :");
+string designation = Console.ReadLine().ToLower();
+
+while (designation.isValidDesignantion())
+{
+    Console.WriteLine("Enter Designation again, not valid designantion");
+    designation = Console.ReadLine();
+}
+
+Console.WriteLine("Enter Salary :");
+Int32 salary = Convert.ToInt32(Console.ReadLine());
+while (salary.isValidSalary())
+{
+    Console.WriteLine("Not Valid Salary, Enter Again"); 
+    salary = Convert.ToInt32(Console.ReadLine());
+}
+
+
+
+
+
