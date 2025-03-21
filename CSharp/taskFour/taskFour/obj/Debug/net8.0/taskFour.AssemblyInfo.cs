45c1e0c48cdf424cc2ca86bf8a65cfea60a306dc
jaydipchangani <pjaydip956@gmail.com>
2025-03-21 12:50:45 +0530

    UI change for form and provacy tab removes

--- CSharp/taskFour/taskFour/obj/Debug/net8.0/taskFour.AssemblyInfo.cs
+++ CSharp/taskFour/taskFour/obj/Debug/net8.0/taskFour.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("taskFour")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ba28764fe7f6c10d1c3a26d8ddcee58443b152e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5cbd0eea9108c6dfb0f8008d8c05ee37b16ae20")]
 [assembly: System.Reflection.AssemblyProductAttribute("taskFour")]
 [assembly: System.Reflection.AssemblyTitleAttribute("taskFour")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
