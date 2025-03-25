abefba4d8147324f1708fbab2036d9e8b04eafd5
jaydipchangani <pjaydip956@gmail.com>
2025-03-25 11:35:04 +0530

    prj init and db connected

---
+++ CSharp/TaskSix/TaskSix/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TaskSix.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
